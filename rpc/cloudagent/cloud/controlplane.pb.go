890c5b288e0aad5734f43082889a1e8e65b47270
Brian Fjeldstad <bfjelds@microsoft.com>
2020-09-10 16:24:18 -0700

    update cloudagent/cloud/controlplane API

--- rpc/cloudagent/cloud/controlplane.pb.go
+++ rpc/cloudagent/cloud/controlplane.pb.go
@@ -73,53 +73,6 @@ func (m *ControlPlaneRequest) GetOperationType() common.Operation {
 	return common.Operation_GET
 }
 
-type ControlPlanePeerRequest struct {
-	ControlPlanes        []*ControlPlane `protobuf:"bytes,1,rep,name=ControlPlanes,proto3" json:"ControlPlanes,omitempty"`
-	Peer                 *Peer           `protobuf:"bytes,2,opt,name=Peer,proto3" json:"Peer,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}        `json:"-"`
-	XXX_unrecognized     []byte          `json:"-"`
-	XXX_sizecache        int32           `json:"-"`
-}
-
-func (m *ControlPlanePeerRequest) Reset()         { *m = ControlPlanePeerRequest{} }
-func (m *ControlPlanePeerRequest) String() string { return proto.CompactTextString(m) }
-func (*ControlPlanePeerRequest) ProtoMessage()    {}
-func (*ControlPlanePeerRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e5f0814968a0baa7, []int{1}
-}
-
-func (m *ControlPlanePeerRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_ControlPlanePeerRequest.Unmarshal(m, b)
-}
-func (m *ControlPlanePeerRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_ControlPlanePeerRequest.Marshal(b, m, deterministic)
-}
-func (m *ControlPlanePeerRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_ControlPlanePeerRequest.Merge(m, src)
-}
-func (m *ControlPlanePeerRequest) XXX_Size() int {
-	return xxx_messageInfo_ControlPlanePeerRequest.Size(m)
-}
-func (m *ControlPlanePeerRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_ControlPlanePeerRequest.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_ControlPlanePeerRequest proto.InternalMessageInfo
-
-func (m *ControlPlanePeerRequest) GetControlPlanes() []*ControlPlane {
-	if m != nil {
-		return m.ControlPlanes
-	}
-	return nil
-}
-
-func (m *ControlPlanePeerRequest) GetPeer() *Peer {
-	if m != nil {
-		return m.Peer
-	}
-	return nil
-}
-
 type ControlPlaneResponse struct {
 	ControlPlanes        []*ControlPlane     `protobuf:"bytes,1,rep,name=ControlPlanes,proto3" json:"ControlPlanes,omitempty"`
 	Result               *wrappers.BoolValue `protobuf:"bytes,2,opt,name=Result,proto3" json:"Result,omitempty"`
@@ -133,7 +86,7 @@ func (m *ControlPlaneResponse) Reset()         { *m = ControlPlaneResponse{} }
 func (m *ControlPlaneResponse) String() string { return proto.CompactTextString(m) }
 func (*ControlPlaneResponse) ProtoMessage()    {}
 func (*ControlPlaneResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e5f0814968a0baa7, []int{2}
+	return fileDescriptor_e5f0814968a0baa7, []int{1}
 }
 
 func (m *ControlPlaneResponse) XXX_Unmarshal(b []byte) error {
@@ -177,12 +130,14 @@ func (m *ControlPlaneResponse) GetError() string {
 
 type ControlPlane struct {
 	Id                   string         `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-	LocationName         string         `protobuf:"bytes,2,opt,name=locationName,proto3" json:"locationName,omitempty"`
-	Peers                []*Peer        `protobuf:"bytes,3,rep,name=peers,proto3" json:"peers,omitempty"`
-	LeaderIp             string         `protobuf:"bytes,4,opt,name=leaderIp,proto3" json:"leaderIp,omitempty"`
-	LeaderPort           string         `protobuf:"bytes,5,opt,name=leaderPort,proto3" json:"leaderPort,omitempty"`
-	LeaderFqdn           string         `protobuf:"bytes,6,opt,name=leaderFqdn,proto3" json:"leaderFqdn,omitempty"`
-	Status               *common.Status `protobuf:"bytes,7,opt,name=status,proto3" json:"status,omitempty"`
+	Name                 string         `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	LocationName         string         `protobuf:"bytes,3,opt,name=locationName,proto3" json:"locationName,omitempty"`
+	Fqdn                 string         `protobuf:"bytes,4,opt,name=fqdn,proto3" json:"fqdn,omitempty"`
+	Port                 int32          `protobuf:"varint,5,opt,name=port,proto3" json:"port,omitempty"`
+	Certificate          string         `protobuf:"bytes,6,opt,name=certificate,proto3" json:"certificate,omitempty"`
+	AuthorizerPort       int32          `protobuf:"varint,7,opt,name=authorizerPort,proto3" json:"authorizerPort,omitempty"`
+	IsLeader             bool           `protobuf:"varint,8,opt,name=isLeader,proto3" json:"isLeader,omitempty"`
+	Status               *common.Status `protobuf:"bytes,9,opt,name=status,proto3" json:"status,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
 	XXX_unrecognized     []byte         `json:"-"`
 	XXX_sizecache        int32          `json:"-"`
@@ -192,7 +147,7 @@ func (m *ControlPlane) Reset()         { *m = ControlPlane{} }
 func (m *ControlPlane) String() string { return proto.CompactTextString(m) }
 func (*ControlPlane) ProtoMessage()    {}
 func (*ControlPlane) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e5f0814968a0baa7, []int{3}
+	return fileDescriptor_e5f0814968a0baa7, []int{2}
 }
 
 func (m *ControlPlane) XXX_Unmarshal(b []byte) error {
@@ -220,6 +175,13 @@ func (m *ControlPlane) GetId() string {
 	return ""
 }
 
+func (m *ControlPlane) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
 func (m *ControlPlane) GetLocationName() string {
 	if m != nil {
 		return m.LocationName
@@ -227,32 +189,39 @@ func (m *ControlPlane) GetLocationName() string {
 	return ""
 }
 
-func (m *ControlPlane) GetPeers() []*Peer {
+func (m *ControlPlane) GetFqdn() string {
 	if m != nil {
-		return m.Peers
-	}
-	return nil
-}
-
-func (m *ControlPlane) GetLeaderIp() string {
-	if m != nil {
-		return m.LeaderIp
+		return m.Fqdn
 	}
 	return ""
 }
 
-func (m *ControlPlane) GetLeaderPort() string {
+func (m *ControlPlane) GetPort() int32 {
 	if m != nil {
-		return m.LeaderPort
+		return m.Port
+	}
+	return 0
+}
+
+func (m *ControlPlane) GetCertificate() string {
+	if m != nil {
+		return m.Certificate
 	}
 	return ""
 }
 
-func (m *ControlPlane) GetLeaderFqdn() string {
+func (m *ControlPlane) GetAuthorizerPort() int32 {
 	if m != nil {
-		return m.LeaderFqdn
+		return m.AuthorizerPort
 	}
-	return ""
+	return 0
+}
+
+func (m *ControlPlane) GetIsLeader() bool {
+	if m != nil {
+		return m.IsLeader
+	}
+	return false
 }
 
 func (m *ControlPlane) GetStatus() *common.Status {
@@ -262,96 +231,44 @@ func (m *ControlPlane) GetStatus() *common.Status {
 	return nil
 }
 
-type Peer struct {
-	Fqdn                 string   `protobuf:"bytes,1,opt,name=fqdn,proto3" json:"fqdn,omitempty"`
-	Port                 string   `protobuf:"bytes,2,opt,name=port,proto3" json:"port,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *Peer) Reset()         { *m = Peer{} }
-func (m *Peer) String() string { return proto.CompactTextString(m) }
-func (*Peer) ProtoMessage()    {}
-func (*Peer) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e5f0814968a0baa7, []int{4}
-}
-
-func (m *Peer) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Peer.Unmarshal(m, b)
-}
-func (m *Peer) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Peer.Marshal(b, m, deterministic)
-}
-func (m *Peer) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Peer.Merge(m, src)
-}
-func (m *Peer) XXX_Size() int {
-	return xxx_messageInfo_Peer.Size(m)
-}
-func (m *Peer) XXX_DiscardUnknown() {
-	xxx_messageInfo_Peer.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_Peer proto.InternalMessageInfo
-
-func (m *Peer) GetFqdn() string {
-	if m != nil {
-		return m.Fqdn
-	}
-	return ""
-}
-
-func (m *Peer) GetPort() string {
-	if m != nil {
-		return m.Port
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*ControlPlaneRequest)(nil), "moc.cloudagent.controlplane.ControlPlaneRequest")
-	proto.RegisterType((*ControlPlanePeerRequest)(nil), "moc.cloudagent.controlplane.ControlPlanePeerRequest")
 	proto.RegisterType((*ControlPlaneResponse)(nil), "moc.cloudagent.controlplane.ControlPlaneResponse")
 	proto.RegisterType((*ControlPlane)(nil), "moc.cloudagent.controlplane.ControlPlane")
-	proto.RegisterType((*Peer)(nil), "moc.cloudagent.controlplane.Peer")
 }
 
 func init() { proto.RegisterFile("controlplane.proto", fileDescriptor_e5f0814968a0baa7) }
 
 var fileDescriptor_e5f0814968a0baa7 = []byte{
-	// 487 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xbc, 0x53, 0xcd, 0x6e, 0xd3, 0x40,
-	0x10, 0xc6, 0x69, 0xe2, 0xd2, 0x49, 0x1a, 0x89, 0xa5, 0x12, 0x96, 0x91, 0xaa, 0x60, 0x2e, 0xe1,
-	0xc0, 0x1a, 0x4c, 0x11, 0xe7, 0x16, 0x81, 0xd4, 0x0b, 0x8d, 0x0c, 0xe2, 0xc0, 0xcd, 0xb1, 0x27,
-	0xc1, 0x62, 0xed, 0xd9, 0xee, 0xae, 0x5b, 0xf1, 0x2e, 0x5c, 0x78, 0x04, 0x9e, 0x8d, 0x2b, 0x07,
-	0xe4, 0xdd, 0xd0, 0xda, 0x97, 0x2a, 0x20, 0xe0, 0x36, 0x3f, 0xdf, 0x7c, 0xf3, 0xcd, 0xcc, 0x2e,
-	0xb0, 0x9c, 0x6a, 0xa3, 0x48, 0x48, 0x91, 0xd5, 0xc8, 0xa5, 0x22, 0x43, 0xec, 0x7e, 0x45, 0x39,
-	0xcf, 0x05, 0x35, 0x45, 0xb6, 0xc6, 0xda, 0xf0, 0x2e, 0x24, 0x3c, 0x5c, 0x13, 0xad, 0x05, 0xc6,
-	0x16, 0xba, 0x6c, 0x56, 0xf1, 0xa5, 0xca, 0xa4, 0x44, 0xa5, 0x5d, 0x71, 0x38, 0xc9, 0xa9, 0xaa,
-	0xa8, 0x76, 0x5e, 0xf4, 0xc5, 0x83, 0xbb, 0x2f, 0x5d, 0xf9, 0xa2, 0x2d, 0x4f, 0xf1, 0xbc, 0x41,
-	0x6d, 0xd8, 0x19, 0xec, 0x77, 0xc3, 0x3a, 0xf0, 0x66, 0x3b, 0xf3, 0x71, 0xf2, 0x88, 0xdf, 0xd0,
-	0x9a, 0xf7, 0x88, 0xfa, 0xf5, 0xec, 0x08, 0xf6, 0xcf, 0x24, 0xaa, 0xcc, 0x94, 0x54, 0xbf, 0xfb,
-	0x2c, 0x31, 0x18, 0xcc, 0xbc, 0xf9, 0x34, 0x99, 0x5a, 0xc2, 0xab, 0x4c, 0xda, 0x07, 0x45, 0x5f,
-	0x3d, 0xb8, 0xd7, 0xe5, 0x59, 0x20, 0xaa, 0x7f, 0x26, 0xf1, 0x39, 0x0c, 0x5b, 0x7e, 0xab, 0x6c,
-	0x9c, 0x3c, 0xb8, 0x91, 0xc7, 0x0a, 0xb1, 0xf0, 0xe8, 0x9b, 0x07, 0x07, 0xfd, 0x15, 0x6a, 0x49,
-	0xb5, 0xc6, 0xbf, 0x2f, 0x30, 0x01, 0x3f, 0x45, 0xdd, 0x08, 0xb3, 0x91, 0x18, 0x72, 0x77, 0x6b,
-	0xfe, 0xeb, 0xd6, 0xfc, 0x84, 0x48, 0xbc, 0xcf, 0x44, 0x83, 0xe9, 0x06, 0xc9, 0x0e, 0x60, 0xf4,
-	0x4a, 0x29, 0x52, 0xc1, 0xce, 0xcc, 0x9b, 0xef, 0xa5, 0xce, 0x89, 0x7e, 0x78, 0x30, 0xe9, 0x72,
-	0xb3, 0x29, 0x0c, 0xca, 0x22, 0xf0, 0x2c, 0x66, 0x50, 0x16, 0x2c, 0x82, 0x89, 0xa0, 0xdc, 0x1e,
-	0xe2, 0x4d, 0x56, 0xb9, 0x6b, 0xed, 0xa5, 0xbd, 0x18, 0x7b, 0x01, 0x23, 0x89, 0xa8, 0x74, 0xb0,
-	0x63, 0xe7, 0xda, 0x62, 0x61, 0x0e, 0xcf, 0x42, 0xb8, 0x2d, 0x30, 0x2b, 0x50, 0x9d, 0xca, 0x60,
-	0x68, 0x89, 0xaf, 0x7c, 0x76, 0x08, 0xe0, 0xec, 0x05, 0x29, 0x13, 0x8c, 0x6c, 0xb6, 0x13, 0xb9,
-	0xce, 0xbf, 0x3e, 0x2f, 0xea, 0xc0, 0xef, 0xe6, 0xdb, 0x08, 0x7b, 0x08, 0xbe, 0x36, 0x99, 0x69,
-	0x74, 0xb0, 0x6b, 0x77, 0x34, 0xb6, 0xaa, 0xde, 0xda, 0x50, 0xba, 0x49, 0x45, 0xdc, 0x5d, 0x9a,
-	0x31, 0x18, 0xae, 0x5a, 0x1a, 0x37, 0xb7, 0xb5, 0xdb, 0x98, 0x6c, 0x5b, 0xbb, 0x89, 0xad, 0x9d,
-	0x7c, 0x1f, 0xc0, 0x9d, 0xee, 0xba, 0x8e, 0xdb, 0xf9, 0x18, 0x81, 0x7f, 0x5a, 0x5f, 0xd0, 0x27,
-	0x64, 0x4f, 0xb6, 0x3f, 0xa9, 0x7b, 0xbc, 0xe1, 0xd3, 0xdf, 0xa8, 0x70, 0xcf, 0x29, 0xba, 0xc5,
-	0x0c, 0xec, 0x1e, 0x17, 0x85, 0x55, 0x7e, 0xb4, 0x75, 0x7d, 0xe7, 0xcb, 0xfc, 0x59, 0xd7, 0x4b,
-	0x80, 0x14, 0x2b, 0xba, 0xc0, 0xff, 0xdc, 0xf8, 0x24, 0xfe, 0xf0, 0x78, 0x5d, 0x9a, 0x8f, 0xcd,
-	0x92, 0xe7, 0x54, 0xc5, 0x55, 0x99, 0x2b, 0xd2, 0xb4, 0x32, 0x71, 0x45, 0x79, 0xac, 0x64, 0x1e,
-	0x5f, 0xd3, 0x39, 0x73, 0xe9, 0xdb, 0x7f, 0xf0, 0xec, 0x67, 0x00, 0x00, 0x00, 0xff, 0xff, 0x24,
-	0x23, 0x8e, 0xd7, 0x34, 0x05, 0x00, 0x00,
+	// 442 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x52, 0xc1, 0x6e, 0xd4, 0x30,
+	0x10, 0x25, 0xdb, 0x6e, 0xd8, 0xf5, 0x6e, 0x57, 0xc2, 0xf4, 0x60, 0x05, 0x09, 0x45, 0x41, 0x42,
+	0xe1, 0x80, 0x03, 0x81, 0x1f, 0xa0, 0x88, 0x03, 0x12, 0xa2, 0x95, 0x41, 0x1c, 0xb8, 0x79, 0x1d,
+	0xef, 0xd6, 0x22, 0xf1, 0xb8, 0xb6, 0x03, 0x82, 0x33, 0x17, 0xfe, 0x81, 0x9f, 0xe0, 0x0f, 0x51,
+	0xec, 0x52, 0x12, 0x0e, 0x08, 0xa4, 0xde, 0x66, 0xde, 0xbc, 0xf7, 0x32, 0x79, 0x63, 0x84, 0x05,
+	0x68, 0x6f, 0xa1, 0x35, 0x2d, 0xd7, 0x92, 0x1a, 0x0b, 0x1e, 0xf0, 0x9d, 0x0e, 0x04, 0x15, 0x2d,
+	0xf4, 0x0d, 0xdf, 0x4b, 0xed, 0xe9, 0x98, 0x92, 0xdd, 0xdd, 0x03, 0xec, 0x5b, 0x59, 0x05, 0xea,
+	0xb6, 0xdf, 0x55, 0x9f, 0x2c, 0x37, 0x46, 0x5a, 0x17, 0xc5, 0xd9, 0x5a, 0x40, 0xd7, 0x81, 0x8e,
+	0x5d, 0xf1, 0x3d, 0x41, 0xb7, 0x9f, 0x47, 0xf9, 0xd9, 0x20, 0x67, 0xf2, 0xa2, 0x97, 0xce, 0xe3,
+	0x53, 0x74, 0x34, 0x86, 0x1d, 0x49, 0xf2, 0x83, 0x72, 0x55, 0x3f, 0xa0, 0x7f, 0xf9, 0x34, 0x9d,
+	0x18, 0x4d, 0xf5, 0xf8, 0x29, 0x3a, 0x3a, 0x35, 0xd2, 0x72, 0xaf, 0x40, 0xbf, 0xfd, 0x6c, 0x24,
+	0x99, 0xe5, 0x49, 0xb9, 0xa9, 0x37, 0xc1, 0xf0, 0x6a, 0xc2, 0xa6, 0xa4, 0xe2, 0x47, 0x82, 0x8e,
+	0xa7, 0xeb, 0x39, 0x03, 0xda, 0xc9, 0xeb, 0xdf, 0xaf, 0x46, 0x29, 0x93, 0xae, 0x6f, 0x7d, 0x58,
+	0x6c, 0x55, 0x67, 0x34, 0xe6, 0x48, 0x7f, 0xe5, 0x48, 0x4f, 0x00, 0xda, 0x77, 0xbc, 0xed, 0x25,
+	0xbb, 0x64, 0xe2, 0x63, 0x34, 0x7f, 0x61, 0x2d, 0x58, 0x72, 0x90, 0x27, 0xe5, 0x92, 0xc5, 0xa6,
+	0xf8, 0x36, 0x43, 0xeb, 0xb1, 0x37, 0xde, 0xa0, 0x99, 0x6a, 0x48, 0x12, 0x38, 0x33, 0xd5, 0x60,
+	0x8c, 0x0e, 0x35, 0xef, 0x62, 0x02, 0x4b, 0x16, 0x6a, 0x5c, 0xa0, 0x75, 0x0b, 0x22, 0xfc, 0xf8,
+	0xeb, 0x61, 0x16, 0x1d, 0x27, 0xd8, 0xa0, 0xdb, 0x5d, 0x34, 0x9a, 0x1c, 0x46, 0xdd, 0x50, 0x0f,
+	0x98, 0x01, 0xeb, 0xc9, 0x3c, 0x4f, 0xca, 0x39, 0x0b, 0x35, 0xce, 0xd1, 0x4a, 0x48, 0xeb, 0xd5,
+	0x4e, 0x09, 0xee, 0x25, 0x49, 0x03, 0x7d, 0x0c, 0xe1, 0xfb, 0x68, 0xc3, 0x7b, 0x7f, 0x0e, 0x56,
+	0x7d, 0x91, 0xf6, 0x6c, 0xd0, 0xdf, 0x0c, 0xfa, 0x3f, 0x50, 0x9c, 0xa1, 0x85, 0x72, 0xaf, 0x24,
+	0x6f, 0xa4, 0x25, 0x8b, 0x3c, 0x29, 0x17, 0xec, 0xaa, 0xc7, 0xf7, 0x50, 0xea, 0x3c, 0xf7, 0xbd,
+	0x23, 0xcb, 0x10, 0xd8, 0x2a, 0x44, 0xff, 0x26, 0x40, 0xec, 0x72, 0x54, 0x7f, 0x4d, 0xd0, 0xad,
+	0x71, 0x16, 0xcf, 0x86, 0xa3, 0x60, 0x40, 0xe9, 0x4b, 0xfd, 0x11, 0x3e, 0x48, 0xfc, 0xe8, 0xdf,
+	0xef, 0x15, 0x1f, 0x66, 0xf6, 0xf8, 0x3f, 0x14, 0xf1, 0xad, 0x14, 0x37, 0x4e, 0xaa, 0xf7, 0x0f,
+	0xf7, 0xca, 0x9f, 0xf7, 0x5b, 0x2a, 0xa0, 0xab, 0x3a, 0x25, 0x2c, 0x38, 0xd8, 0xf9, 0xaa, 0x03,
+	0x51, 0x59, 0x23, 0xaa, 0xdf, 0x76, 0xb1, 0xdc, 0xa6, 0xe1, 0xea, 0x4f, 0x7e, 0x06, 0x00, 0x00,
+	0xff, 0xff, 0xfe, 0x67, 0xa0, 0x47, 0x7e, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -367,8 +284,6 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ControlPlaneAgentClient interface {
 	Invoke(ctx context.Context, in *ControlPlaneRequest, opts ...grpc.CallOption) (*ControlPlaneResponse, error)
-	AddPeer(ctx context.Context, in *ControlPlanePeerRequest, opts ...grpc.CallOption) (*ControlPlaneResponse, error)
-	RemovePeer(ctx context.Context, in *ControlPlanePeerRequest, opts ...grpc.CallOption) (*ControlPlaneResponse, error)
 }
 
 type controlPlaneAgentClient struct {
@@ -388,29 +303,9 @@ func (c *controlPlaneAgentClient) Invoke(ctx context.Context, in *ControlPlaneRe
 	return out, nil
 }
 
-func (c *controlPlaneAgentClient) AddPeer(ctx context.Context, in *ControlPlanePeerRequest, opts ...grpc.CallOption) (*ControlPlaneResponse, error) {
-	out := new(ControlPlaneResponse)
-	err := c.cc.Invoke(ctx, "/moc.cloudagent.controlplane.ControlPlaneAgent/AddPeer", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *controlPlaneAgentClient) RemovePeer(ctx context.Context, in *ControlPlanePeerRequest, opts ...grpc.CallOption) (*ControlPlaneResponse, error) {
-	out := new(ControlPlaneResponse)
-	err := c.cc.Invoke(ctx, "/moc.cloudagent.controlplane.ControlPlaneAgent/RemovePeer", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // ControlPlaneAgentServer is the server API for ControlPlaneAgent service.
 type ControlPlaneAgentServer interface {
 	Invoke(context.Context, *ControlPlaneRequest) (*ControlPlaneResponse, error)
-	AddPeer(context.Context, *ControlPlanePeerRequest) (*ControlPlaneResponse, error)
-	RemovePeer(context.Context, *ControlPlanePeerRequest) (*ControlPlaneResponse, error)
 }
 
 // UnimplementedControlPlaneAgentServer can be embedded to have forward compatible implementations.
@@ -419,12 +314,6 @@ type UnimplementedControlPlaneAgentServer struct {
 
 func (*UnimplementedControlPlaneAgentServer) Invoke(ctx context.Context, req *ControlPlaneRequest) (*ControlPlaneResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Invoke not implemented")
-}
-func (*UnimplementedControlPlaneAgentServer) AddPeer(ctx context.Context, req *ControlPlanePeerRequest) (*ControlPlaneResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddPeer not implemented")
-}
-func (*UnimplementedControlPlaneAgentServer) RemovePeer(ctx context.Context, req *ControlPlanePeerRequest) (*ControlPlaneResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method RemovePeer not implemented")
 }
 
 func RegisterControlPlaneAgentServer(s *grpc.Server, srv ControlPlaneAgentServer) {
@@ -449,42 +338,6 @@ func _ControlPlaneAgent_Invoke_Handler(srv interface{}, ctx context.Context, dec
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ControlPlaneAgent_AddPeer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ControlPlanePeerRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ControlPlaneAgentServer).AddPeer(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/moc.cloudagent.controlplane.ControlPlaneAgent/AddPeer",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ControlPlaneAgentServer).AddPeer(ctx, req.(*ControlPlanePeerRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _ControlPlaneAgent_RemovePeer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ControlPlanePeerRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ControlPlaneAgentServer).RemovePeer(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/moc.cloudagent.controlplane.ControlPlaneAgent/RemovePeer",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ControlPlaneAgentServer).RemovePeer(ctx, req.(*ControlPlanePeerRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 var _ControlPlaneAgent_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "moc.cloudagent.controlplane.ControlPlaneAgent",
 	HandlerType: (*ControlPlaneAgentServer)(nil),
@@ -492,14 +345,6 @@ var _ControlPlaneAgent_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Invoke",
 			Handler:    _ControlPlaneAgent_Invoke_Handler,
-		},
-		{
-			MethodName: "AddPeer",
-			Handler:    _ControlPlaneAgent_AddPeer_Handler,
-		},
-		{
-			MethodName: "RemovePeer",
-			Handler:    _ControlPlaneAgent_RemovePeer_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
