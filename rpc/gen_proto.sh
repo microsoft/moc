382e4ef7df680cc7a2be8f94bcce0827d03b7dc0
Jiri Appl <jiria@microsoft.com>
2021-01-08 02:34:04 -0800

    Split etcd configuration into separate objects

--- rpc/gen_proto.sh
+++ rpc/gen_proto.sh
@@ -121,7 +121,8 @@ protoc -I $Agent/$Module/cluster -I $Agent/$Module/node -I ./common $Agent/$Modu
 protoc -I $Agent/$Module/location -I ./common $Agent/$Module/location/moc_cloudagent_location.proto  --go_out=plugins=grpc:../bld/gen/
 protoc -I $Agent/$Module/subscription -I ./common $Agent/$Module/subscription/moc_cloudagent_subscription.proto  --go_out=plugins=grpc:../bld/gen/
 protoc -I $Agent/$Module/controlplane -I ./common $Agent/$Module/controlplane/moc_cloudagent_controlplane.proto  --go_out=plugins=grpc:../bld/gen/
-
+protoc -I $Agent/$Module/etcdcluster -I ./common $Agent/$Module/etcdcluster/moc_cloudagent_etcdcluster.proto  --go_out=plugins=grpc:../bld/gen/
+protoc -I $Agent/$Module/etcdpeer -I ./common $Agent/$Module/etcdpeer/moc_cloudagent_etcdpeer.proto  --go_out=plugins=grpc:../bld/gen/
 
 Module="security"
 echo "Generating $Agent/$Module protoc"
