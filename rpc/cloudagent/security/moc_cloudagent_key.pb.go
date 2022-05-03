d0eb72c7776367d2c41e8cdb97cbc8cbae81f4ef
Niranjan Shinde <niranjanshnd@gmail.com>
2022-05-03 12:41:00 -0700

    No plaintext import

--- rpc/cloudagent/security/moc_cloudagent_key.pb.go
+++ rpc/cloudagent/security/moc_cloudagent_key.pb.go
@@ -298,7 +298,7 @@ func (m *PrivateKeyWrappingInfo) GetWrappingAlgorithm() common.KeyWrappingAlgori
 	if m != nil {
 		return m.WrappingAlgorithm
 	}
-	return common.KeyWrappingAlgorithm_NO_KEY_WRAP
+	return common.KeyWrappingAlgorithm_CKM_RSA_AES_KEY_WRAP
 }
 
 type Key struct {
