315d68cffed1c254ecb9620ed81642e2d0653f38
HariPrasadV <hariv97@gmail.com>
2023-02-16 15:50:48 -0800

    Added NodeOutOfCapacityError

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -28,6 +28,7 @@ var (
 	InvalidVersion       error = errors.New("InvalidVersion")
 	OldVersion           error = errors.New("OldVersion")
 	OutOfCapacity        error = errors.New("OutOfCapacity")
+	NodeOutOfCapacity    error = errors.New("NodeOutOfCapacity")
 	OutOfMemory          error = errors.New("OutOfMemory")
 	UpdateFailed         error = errors.New("Update Failed")
 	NotInitialized       error = errors.New("Not Initialized")
@@ -79,6 +80,8 @@ func GetErrorCode(err error) string {
 		return "OldVersion"
 	} else if IsOutOfCapacity(err) {
 		return "OutOfCapacity"
+	} else if IsNodeOutOfCapacity(err) {
+		return "NodeOutOfCapacity"
 	} else if IsOutOfMemory(err) {
 		return "OutOfMemory"
 	} else if IsUpdateFailed(err) {
@@ -220,6 +223,9 @@ func IsOutOfRange(err error) bool {
 }
 func IsOutOfCapacity(err error) bool {
 	return checkError(err, OutOfCapacity)
+}
+func IsNodeOutOfCapacity(err error) bool {
+	return checkError(err, NodeOutOfCapacity)
 }
 func IsAlreadySet(err error) bool {
 	return checkError(err, AlreadySet)
