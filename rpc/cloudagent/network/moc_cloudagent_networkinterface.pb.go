5a699be3999d3a9e2c60fd8eea06c131800b3467
mjethwa-msft <mjethwa@microsoft.com>
2023-06-27 15:44:13 -0700

    Compile Proto files

--- rpc/cloudagent/network/moc_cloudagent_networkinterface.pb.go
+++ rpc/cloudagent/network/moc_cloudagent_networkinterface.pb.go
@@ -6,13 +6,15 @@ package network
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	wrappers "github.com/golang/protobuf/ptypes/wrappers"
-	common "github.com/microsoft/moc/rpc/common"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	math "math"
+
+	common "github.com/microsoft/moc/rpc/common"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
