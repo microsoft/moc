cec692533a45d7ce7270e0582eb8cad08913b635
Alexander Gama Espinosa <algamaes@microsoft.com>
2023-08-22 16:57:46 -0700

    Fix format errors

--- rpc/mocguestagent/admin/moc_mocguestagent_health.pb.go
+++ rpc/mocguestagent/admin/moc_mocguestagent_health.pb.go
@@ -26,46 +26,46 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type HealthAgentRequest struct {
+type HealthRequest struct {
 	TimeoutSeconds       uint32   `protobuf:"varint,1,opt,name=timeoutSeconds,proto3" json:"timeoutSeconds,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *HealthAgentRequest) Reset()         { *m = HealthAgentRequest{} }
-func (m *HealthAgentRequest) String() string { return proto.CompactTextString(m) }
-func (*HealthAgentRequest) ProtoMessage()    {}
-func (*HealthAgentRequest) Descriptor() ([]byte, []int) {
+func (m *HealthRequest) Reset()         { *m = HealthRequest{} }
+func (m *HealthRequest) String() string { return proto.CompactTextString(m) }
+func (*HealthRequest) ProtoMessage()    {}
+func (*HealthRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_e02470f27938728d, []int{0}
 }
 
-func (m *HealthAgentRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_HealthAgentRequest.Unmarshal(m, b)
+func (m *HealthRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_HealthRequest.Unmarshal(m, b)
 }
-func (m *HealthAgentRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_HealthAgentRequest.Marshal(b, m, deterministic)
+func (m *HealthRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_HealthRequest.Marshal(b, m, deterministic)
 }
-func (m *HealthAgentRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthAgentRequest.Merge(m, src)
+func (m *HealthRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_HealthRequest.Merge(m, src)
 }
-func (m *HealthAgentRequest) XXX_Size() int {
-	return xxx_messageInfo_HealthAgentRequest.Size(m)
+func (m *HealthRequest) XXX_Size() int {
+	return xxx_messageInfo_HealthRequest.Size(m)
 }
-func (m *HealthAgentRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthAgentRequest.DiscardUnknown(m)
+func (m *HealthRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_HealthRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_HealthAgentRequest proto.InternalMessageInfo
+var xxx_messageInfo_HealthRequest proto.InternalMessageInfo
 
-func (m *HealthAgentRequest) GetTimeoutSeconds() uint32 {
+func (m *HealthRequest) GetTimeoutSeconds() uint32 {
 	if m != nil {
 		return m.TimeoutSeconds
 	}
 	return 0
 }
 
-type HealthAgentResponse struct {
+type HealthResponse struct {
 	Result                  *wrappers.BoolValue `protobuf:"bytes,1,opt,name=Result,proto3" json:"Result,omitempty"`
 	Error                   string              `protobuf:"bytes,2,opt,name=Error,proto3" json:"Error,omitempty"`
 	State                   common.HealthState  `protobuf:"varint,3,opt,name=State,proto3,enum=moc.HealthState" json:"State,omitempty"`
@@ -75,53 +75,53 @@ type HealthAgentResponse struct {
 	XXX_sizecache           int32               `json:"-"`
 }
 
-func (m *HealthAgentResponse) Reset()         { *m = HealthAgentResponse{} }
-func (m *HealthAgentResponse) String() string { return proto.CompactTextString(m) }
-func (*HealthAgentResponse) ProtoMessage()    {}
-func (*HealthAgentResponse) Descriptor() ([]byte, []int) {
+func (m *HealthResponse) Reset()         { *m = HealthResponse{} }
+func (m *HealthResponse) String() string { return proto.CompactTextString(m) }
+func (*HealthResponse) ProtoMessage()    {}
+func (*HealthResponse) Descriptor() ([]byte, []int) {
 	return fileDescriptor_e02470f27938728d, []int{1}
 }
 
-func (m *HealthAgentResponse) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_HealthAgentResponse.Unmarshal(m, b)
+func (m *HealthResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_HealthResponse.Unmarshal(m, b)
 }
-func (m *HealthAgentResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_HealthAgentResponse.Marshal(b, m, deterministic)
+func (m *HealthResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_HealthResponse.Marshal(b, m, deterministic)
 }
-func (m *HealthAgentResponse) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_HealthAgentResponse.Merge(m, src)
+func (m *HealthResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_HealthResponse.Merge(m, src)
 }
-func (m *HealthAgentResponse) XXX_Size() int {
-	return xxx_messageInfo_HealthAgentResponse.Size(m)
+func (m *HealthResponse) XXX_Size() int {
+	return xxx_messageInfo_HealthResponse.Size(m)
 }
-func (m *HealthAgentResponse) XXX_DiscardUnknown() {
-	xxx_messageInfo_HealthAgentResponse.DiscardUnknown(m)
+func (m *HealthResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_HealthResponse.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_HealthAgentResponse proto.InternalMessageInfo
+var xxx_messageInfo_HealthResponse proto.InternalMessageInfo
 
-func (m *HealthAgentResponse) GetResult() *wrappers.BoolValue {
+func (m *HealthResponse) GetResult() *wrappers.BoolValue {
 	if m != nil {
 		return m.Result
 	}
 	return nil
 }
 
-func (m *HealthAgentResponse) GetError() string {
+func (m *HealthResponse) GetError() string {
 	if m != nil {
 		return m.Error
 	}
 	return ""
 }
 
-func (m *HealthAgentResponse) GetState() common.HealthState {
+func (m *HealthResponse) GetState() common.HealthState {
 	if m != nil {
 		return m.State
 	}
 	return common.HealthState_NOTKNOWN
 }
 
-func (m *HealthAgentResponse) GetCertificateNeedsRenewal() *wrappers.BoolValue {
+func (m *HealthResponse) GetCertificateNeedsRenewal() *wrappers.BoolValue {
 	if m != nil {
 		return m.CertificateNeedsRenewal
 	}
@@ -129,35 +129,35 @@ func (m *HealthAgentResponse) GetCertificateNeedsRenewal() *wrappers.BoolValue {
 }
 
 func init() {
-	proto.RegisterType((*HealthAgentRequest)(nil), "moc.mocguestagent.admin.HealthAgentRequest")
-	proto.RegisterType((*HealthAgentResponse)(nil), "moc.mocguestagent.admin.HealthAgentResponse")
+	proto.RegisterType((*HealthRequest)(nil), "moc.mocguestagent.admin.HealthRequest")
+	proto.RegisterType((*HealthResponse)(nil), "moc.mocguestagent.admin.HealthResponse")
 }
 
 func init() { proto.RegisterFile("moc_mocguestagent_health.proto", fileDescriptor_e02470f27938728d) }
 
 var fileDescriptor_e02470f27938728d = []byte{
-	// 331 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x51, 0xcf, 0x4b, 0xfb, 0x30,
-	0x14, 0xff, 0xf6, 0xab, 0x1b, 0x98, 0xe1, 0x90, 0x28, 0xac, 0xf4, 0x30, 0xc6, 0x0e, 0x63, 0xa0,
-	0x24, 0x52, 0xaf, 0x5e, 0xdc, 0x10, 0x3c, 0x79, 0xc8, 0xc4, 0x83, 0x97, 0x91, 0x65, 0x6f, 0x6d,
-	0xb5, 0xe9, 0xab, 0x49, 0xca, 0xf0, 0x8f, 0xf5, 0x7f, 0x91, 0x36, 0x13, 0xac, 0x32, 0xf4, 0x54,
-	0xfa, 0x3e, 0x3f, 0xde, 0xe7, 0xf3, 0x42, 0x86, 0x1a, 0xd5, 0x52, 0xa3, 0x4a, 0x2a, 0xb0, 0x4e,
-	0x26, 0x50, 0xb8, 0x65, 0x0a, 0x32, 0x77, 0x29, 0x2b, 0x0d, 0x3a, 0xa4, 0x03, 0x8d, 0x8a, 0xb5,
-	0x70, 0x26, 0xd7, 0x3a, 0x2b, 0xa2, 0x61, 0x82, 0x98, 0xe4, 0xc0, 0x1b, 0xda, 0xaa, 0xda, 0xf0,
-	0xad, 0x91, 0x65, 0x09, 0xc6, 0x7a, 0x61, 0x54, 0x0b, 0x97, 0x0a, 0xb5, 0xc6, 0x62, 0xf7, 0xf1,
-	0xc0, 0xf8, 0x9a, 0xd0, 0xbb, 0x66, 0xc3, 0x4d, 0xed, 0x26, 0xe0, 0xb5, 0x76, 0xa6, 0x13, 0xd2,
-	0x77, 0x99, 0x06, 0xac, 0xdc, 0x02, 0x14, 0x16, 0x6b, 0x1b, 0x06, 0xa3, 0x60, 0x7a, 0x2c, 0xbe,
-	0x4d, 0xc7, 0xef, 0x01, 0x39, 0x6d, 0xc9, 0x6d, 0x89, 0x85, 0x05, 0x1a, 0x93, 0xae, 0x00, 0x5b,
-	0xe5, 0xae, 0xd1, 0xf5, 0xe2, 0x88, 0xf9, 0x7c, 0xec, 0x33, 0x1f, 0x9b, 0x21, 0xe6, 0x8f, 0x32,
-	0xaf, 0x40, 0xec, 0x98, 0xf4, 0x8c, 0x74, 0x6e, 0x8d, 0x41, 0x13, 0xfe, 0x1f, 0x05, 0xd3, 0x23,
-	0xe1, 0x7f, 0xe8, 0x84, 0x74, 0x16, 0x4e, 0x3a, 0x08, 0x0f, 0x46, 0xc1, 0xb4, 0x1f, 0x9f, 0xd4,
-	0xed, 0x99, 0x5f, 0xd9, 0xcc, 0x85, 0x87, 0xe9, 0x03, 0x19, 0xcc, 0xc1, 0xb8, 0x6c, 0x93, 0x29,
-	0xe9, 0xe0, 0x1e, 0x60, 0x6d, 0x05, 0x14, 0xb0, 0x95, 0x79, 0x78, 0xf8, 0x6b, 0x84, 0x7d, 0xd2,
-	0xf8, 0x8d, 0xf4, 0xbe, 0xd4, 0xa3, 0xcf, 0xa4, 0x37, 0x4f, 0x41, 0xbd, 0xf8, 0x19, 0x3d, 0x67,
-	0x7b, 0x9e, 0x83, 0xfd, 0x3c, 0x69, 0x74, 0xf1, 0x37, 0xb2, 0x3f, 0xe0, 0xf8, 0xdf, 0x2c, 0x7e,
-	0xba, 0x4c, 0x32, 0x97, 0x56, 0x2b, 0xa6, 0x50, 0x73, 0x9d, 0x29, 0x83, 0x16, 0x37, 0x8e, 0x6b,
-	0x54, 0xdc, 0x94, 0x8a, 0xb7, 0x9c, 0x78, 0xe3, 0xb4, 0xea, 0x36, 0xdd, 0xae, 0x3e, 0x02, 0x00,
-	0x00, 0xff, 0xff, 0xbe, 0x02, 0xac, 0x45, 0x47, 0x02, 0x00, 0x00,
+	// 326 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x51, 0x3f, 0x4f, 0xc2, 0x40,
+	0x14, 0xb7, 0x2a, 0x24, 0x1e, 0x81, 0x98, 0x8b, 0x09, 0x4d, 0x07, 0x42, 0x18, 0x90, 0xe9, 0x6a,
+	0xea, 0xe0, 0x2c, 0xc4, 0xc4, 0xc9, 0xe1, 0x30, 0x0e, 0x2e, 0x78, 0x1c, 0x8f, 0xb6, 0xb1, 0xd7,
+	0x57, 0xef, 0x5e, 0xc3, 0x57, 0xf5, 0xe3, 0x98, 0xf6, 0x60, 0xc0, 0x84, 0x30, 0x35, 0x7d, 0xef,
+	0xf7, 0xef, 0xfd, 0x8e, 0x8d, 0x0c, 0xea, 0x95, 0x41, 0x9d, 0xd6, 0xe0, 0x48, 0xa5, 0x50, 0xd2,
+	0x2a, 0x03, 0x55, 0x50, 0x26, 0x2a, 0x8b, 0x84, 0x7c, 0x68, 0x50, 0x8b, 0xa3, 0xbd, 0x50, 0x1b,
+	0x93, 0x97, 0xd1, 0x28, 0x45, 0x4c, 0x0b, 0x88, 0x5b, 0xd8, 0xba, 0xde, 0xc6, 0x3b, 0xab, 0xaa,
+	0x0a, 0xac, 0xf3, 0xc4, 0xa8, 0x21, 0xae, 0x34, 0x1a, 0x83, 0xe5, 0xfe, 0xe3, 0x17, 0x93, 0x27,
+	0xd6, 0x7f, 0x6d, 0x1d, 0x24, 0xfc, 0x34, 0xa2, 0x7c, 0xca, 0x06, 0x94, 0x1b, 0xc0, 0x9a, 0x96,
+	0xa0, 0xb1, 0xdc, 0xb8, 0x30, 0x18, 0x07, 0xb3, 0xbe, 0xfc, 0x37, 0x9d, 0xfc, 0x06, 0x6c, 0x70,
+	0x60, 0xba, 0x0a, 0x4b, 0x07, 0x3c, 0x61, 0x5d, 0x09, 0xae, 0x2e, 0xa8, 0xa5, 0xf4, 0x92, 0x48,
+	0xf8, 0x54, 0xe2, 0x90, 0x4a, 0xcc, 0x11, 0x8b, 0x0f, 0x55, 0xd4, 0x20, 0xf7, 0x48, 0x7e, 0xc7,
+	0x3a, 0x2f, 0xd6, 0xa2, 0x0d, 0x2f, 0xc7, 0xc1, 0xec, 0x46, 0xfa, 0x1f, 0x3e, 0x65, 0x9d, 0x25,
+	0x29, 0x82, 0xf0, 0x6a, 0x1c, 0xcc, 0x06, 0xc9, 0x6d, 0x73, 0xb3, 0xf0, 0x6e, 0xed, 0x5c, 0xfa,
+	0x35, 0x7f, 0x67, 0xc3, 0x05, 0x58, 0xca, 0xb7, 0xb9, 0x56, 0x04, 0x6f, 0x00, 0x1b, 0x27, 0xa1,
+	0x84, 0x9d, 0x2a, 0xc2, 0xeb, 0xb3, 0x11, 0x4e, 0x51, 0x13, 0x64, 0x3d, 0xef, 0xf5, 0xdc, 0x34,
+	0xcc, 0xbf, 0x58, 0x6f, 0x91, 0x81, 0xfe, 0xf6, 0x33, 0x3e, 0x15, 0x27, 0x1e, 0x41, 0x1c, 0x15,
+	0x19, 0xdd, 0x9f, 0xc5, 0xf9, 0xda, 0x26, 0x17, 0xf3, 0xe4, 0xf3, 0x21, 0xcd, 0x29, 0xab, 0xd7,
+	0x42, 0xa3, 0x89, 0x4d, 0xae, 0x2d, 0x3a, 0xdc, 0x52, 0x6c, 0x50, 0xc7, 0xb6, 0xd2, 0xf1, 0x91,
+	0x48, 0xdc, 0x8a, 0xac, 0xbb, 0xed, 0x45, 0x8f, 0x7f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x71, 0xe7,
+	0x12, 0xf2, 0x33, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -172,7 +172,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type HealthAgentClient interface {
-	CheckHealth(ctx context.Context, in *HealthAgentRequest, opts ...grpc.CallOption) (*HealthAgentResponse, error)
+	CheckHealth(ctx context.Context, in *HealthRequest, opts ...grpc.CallOption) (*HealthResponse, error)
 }
 
 type healthAgentClient struct {
@@ -183,8 +183,8 @@ func NewHealthAgentClient(cc *grpc.ClientConn) HealthAgentClient {
 	return &healthAgentClient{cc}
 }
 
-func (c *healthAgentClient) CheckHealth(ctx context.Context, in *HealthAgentRequest, opts ...grpc.CallOption) (*HealthAgentResponse, error) {
-	out := new(HealthAgentResponse)
+func (c *healthAgentClient) CheckHealth(ctx context.Context, in *HealthRequest, opts ...grpc.CallOption) (*HealthResponse, error) {
+	out := new(HealthResponse)
 	err := c.cc.Invoke(ctx, "/moc.mocguestagent.admin.HealthAgent/CheckHealth", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -194,14 +194,14 @@ func (c *healthAgentClient) CheckHealth(ctx context.Context, in *HealthAgentRequ
 
 // HealthAgentServer is the server API for HealthAgent service.
 type HealthAgentServer interface {
-	CheckHealth(context.Context, *HealthAgentRequest) (*HealthAgentResponse, error)
+	CheckHealth(context.Context, *HealthRequest) (*HealthResponse, error)
 }
 
 // UnimplementedHealthAgentServer can be embedded to have forward compatible implementations.
 type UnimplementedHealthAgentServer struct {
 }
 
-func (*UnimplementedHealthAgentServer) CheckHealth(ctx context.Context, req *HealthAgentRequest) (*HealthAgentResponse, error) {
+func (*UnimplementedHealthAgentServer) CheckHealth(ctx context.Context, req *HealthRequest) (*HealthResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckHealth not implemented")
 }
 
@@ -210,7 +210,7 @@ func RegisterHealthAgentServer(s *grpc.Server, srv HealthAgentServer) {
 }
 
 func _HealthAgent_CheckHealth_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(HealthAgentRequest)
+	in := new(HealthRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -222,7 +222,7 @@ func _HealthAgent_CheckHealth_Handler(srv interface{}, ctx context.Context, dec 
 		FullMethod: "/moc.mocguestagent.admin.HealthAgent/CheckHealth",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HealthAgentServer).CheckHealth(ctx, req.(*HealthAgentRequest))
+		return srv.(HealthAgentServer).CheckHealth(ctx, req.(*HealthRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
