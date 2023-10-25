ca339be294d97ae464f3017aa4bdce39a6614a50
Dhwani Shah <dhwanishah781@gmail.com>
2023-10-25 15:52:04 -0700

    updating test cases

--- pkg/validations/proxy_validation.go
+++ pkg/validations/proxy_validation.go
@@ -11,8 +11,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-
 	"github.com/microsoft/moc/pkg/errors"
+	"time"
 )
 
 func ValidateProxyURL(proxyURL string, certContent string) error {
@@ -22,6 +22,7 @@ func ValidateProxyURL(proxyURL string, certContent string) error {
 		return errors.Wrapf(errors.InvalidInput, err.Error())
 	}
 
+	// Check if url scheme is http or https
 	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
 		return errors.Wrapf(errors.InvalidInput, "Invalid proxy URL. The URL scheme should be http or https")
 	}
@@ -35,6 +36,7 @@ func ValidateProxyURL(proxyURL string, certContent string) error {
 		Proxy: http.ProxyURL(parsedURL),
 		TLSClientConfig: &tls.Config{
 			RootCAs: caCertPool,
+			InsecureSkipVerify: true,
 		},
 	}
 
@@ -55,12 +57,32 @@ func ValidateProxyURL(proxyURL string, certContent string) error {
 	return nil
 }
 
-func ValidateCertFormatIsBase64(certContent string) error {
+func ValidateProxyCertificate(certContent string) error {
 	certContent = strings.Replace(certContent, "-----BEGIN CERTIFICATE-----", "", -1)
 	certContent = strings.Replace(certContent, "-----END CERTIFICATE-----", "", -1)
+
+	// Check if certificate is base64-encoded
 	_, err := base64.StdEncoding.DecodeString(certContent)
+	if err != nil {
+		return errors.Wrapf(errors.InvalidInput, "Proxy server certificate is not base64 encoded. Please provide a base64 encoded certificate.")
+	}
+
+	// Decode the base64-encoded certificate
+	certBytes, _ := base64.StdEncoding.DecodeString(certContent)
+	if certBytes == nil {
+		return errors.Wrapf(errors.InvalidInput, "Failed to decode certificate")
+	}
+	// Parse the decoded certificate
+	caCert, err := x509.ParseCertificate(certBytes)
 	if err != nil {
 		return errors.Wrapf(errors.InvalidInput, err.Error())
 	}
+
+	// Check for the expiry of certificate
+	currentTime := time.Now()
+	if currentTime.After(caCert.NotAfter) {
+		return errors.Wrapf(errors.InvalidInput, "Proxy server SSL/TLS certificate has expired")
+	}
+
 	return nil
 }
--- pkg/validations/proxy_validation_test.go
+++ pkg/validations/proxy_validation_test.go
@@ -4,32 +4,92 @@ package validations
 
 import (
 	"testing"
+	"io/ioutil"
 )
 
 func Test_ValidateProxyURL(t *testing.T) {
-	err := ValidateProxyURL("", "")
+    caCert, err := ioutil.ReadFile("proxy.crt")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+    caCertString := string(caCert)
+
+	// Empty proxy url
+	err = ValidateProxyURL("", "")
 	expectedResult := "parse \"\": empty url: Invalid Input"
 	if err.Error() != expectedResult {
-		t.Fatalf("Test_ValidateProxyURL test case failed. Expected result was %s", expectedResult)
+		t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s but got %s", expectedResult, err.Error())
 	}
 
-	err = ValidateProxyURL("//akse2e:akse2e@skyproxy.ceccloud1.selfhost.corp.microsoft.com:3128", "")
+	// Invalid proxy url
+	err = ValidateProxyURL("//akse2e:akse2e@skyproxy.ceccloud1.selfhost.corp.microsoft.com:3128", caCertString)
 	expectedResult = "Invalid proxy URL. The URL scheme should be http or https: Invalid Input"
 	if err.Error() != expectedResult {
-		t.Fatalf("Test_ValidateProxyURL test case failed. Expected result was %s", expectedResult)
+		t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s but got %s", expectedResult, err.Error())
 	}
 
-	err = ValidateProxyURL("http://akse2e:akse2e@.ceccloud1.selfhost.corp.microsoft.com:3128", "")
+	// Invalid hostname
+	err = ValidateProxyURL("http://akse2e:akse2e@.ceccloud1.selfhost.corp.microsoft.com:3128", caCertString)
 	expectedResult = "Get \"https://mcr.microsoft.com\": proxyconnect tcp: dial tcp: lookup .ceccloud1.selfhost.corp.microsoft.com: no such host: Invalid Input"
 	if err.Error() != expectedResult {
-		t.Fatalf("Test_ValidateProxyURL test case failed. Expected result was %s", expectedResult)
+		t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s but got %s", expectedResult, err.Error())
+	}
+
+	// Unreachable proxy server
+	err = ValidateProxyURL("http://ubuntu:ubuntu@192.168.200.200:3128", "")
+	expectedResult = "Get \"https://mcr.microsoft.com\": proxyconnect tcp: dial tcp 192.168.200.200:3128: connectex: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond.: Invalid Input"
+	if err.Error() != expectedResult {
+		t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s but got %s", expectedResult, err.Error())
+	}
+
+	// Valid case
+	err = ValidateProxyURL("http://akse2e:akse2e@skyproxy.ceccloud1.selfhost.corp.microsoft.com:3128", caCertString)
+	if err != nil {
+		t.Fatalf("Test_ValidateProxyURL test case failed. %s", err.Error())
 	}
 }
 
-func Test_ValidateCertFormatIsBase64(t *testing.T) {
-	certContent := "-----BEGIN CERTIFICATE-----MIIDETCCAfkCFA6m/zJsUNjKnRp++MYKP+WSKpSQMA0GCSqGSIb3DQEBCwUAMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwHhcNMjMxMDEzMTgwNjI1WhcNMjQxMDEyMTgwNjI1WjBFMQswCQYDVQQGEwJBVTETMBEGA1UECAwKU29tZS1TdGF0ZTEhMB8GA1UECgwYSW50ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4HznjiYD16BW7gRvP/rHDpZ5vSVsn1cTrlNtdu3lr3HB3ibNVuF5hLRuA+/5687AmhhSB+ri6+of3R/BR2dSLmbC63qzqC/5JYkZEX7fb6RaiD5twds5EsSHukldLTE+H5lIF7JQhhRG0yz1Tl1BKC7uNSP7CmZOtC8Mf7wCYwwecADtvGE/bJsBLtxb4+QuMOQNV/Ldcb9Oy8ZifIplmNDlW0Jr4QMxffrXj4j1AVMEczNADkFTySEgWtILtvRnQvht6nxE7YBqubRs0zpGfFZFk9DBlwafZIMXXtv1/LNS8k54Fj3+KlXkiWy9jCNaWytJJUGubE3JJzPM1ya/TQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQBi6TEo61Pvf80SCglSKIWszXhFpDzOPP4sy9eVmk6fg4cgt6Lv8ZNPns7U/7zqQLSmxlnhg2MkgGPRG/E9g8437AXd6GaQL+KQfwn+lJy9/xo5ERHZftsAIH4eDkVH42YAzZG8D5M2thdnwiu7JeGoEfrgCXCv7k8ewSTnFk7rH4y/IplVnBjitG93FDzGfnPAo5Oy/F8hX4ht6S3YTW3mvyPJ0z6M5oFZpjVHgG1BLXMG46BEKfVrE1YQ/KkRACPLH1MWDhuLKmjY4ohMLOOuy9c1OT0mhr6FREEBV9zs6bhfLt6UjIMiIqIz+FKPSjZr3EiSAiA8MD4/262q4KGM-----END CERTIFICATE-----"
-	err := ValidateCertFormatIsBase64(certContent)
+func Test_ValidateProxyCertificate(t *testing.T) {
+	caCert, err := ioutil.ReadFile("proxy.crt")
 	if err != nil {
-		t.Fatalf("Test_ValidateCertFormat test case failed - Certificate content is not base64 encoded")
+		t.Fatalf(err.Error())
+	}
+    caCertString := string(caCert)
+
+	// Valid case
+	err = ValidateProxyCertificate(caCertString)
+	if err != nil {
+		t.Fatalf("Test_ValidateProxyCertificate test case failed. %s", err.Error())
+	}
+
+	// Invalid certificate
+	caCert, err = ioutil.ReadFile("sample-invalidCertificate.crt")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	caCertString = string(caCert)
+	err = ValidateProxyCertificate(caCertString)
+	expectedResult := "Proxy server certificate is not base64 encoded. Please provide a base64 encoded certificate.: Invalid Input"
+	if err.Error() != expectedResult {
+		t.Fatalf("Test_ValidateProxyCertificate test case failed. Expected error %s but got %s", expectedResult, err.Error())
+	}
+
+	// Expired certificate
+	caCert, err = ioutil.ReadFile("sample-expiredCertificate.crt")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	caCertString = string(caCert)
+	err = ValidateProxyCertificate(caCertString)
+	expectedResult = "Proxy server SSL/TLS certificate has expired: Invalid Input"
+	if err.Error() != expectedResult {
+		t.Fatalf("Test_ValidateProxyCertificate test case failed. Expected error %s but got %s", expectedResult, err.Error())
+	}
+
+	// Malformed certificate
+	expectedResult = "x509: malformed certificate: Invalid Input"
+	err = ValidateProxyCertificate("")
+	if err.Error() != expectedResult {
+		t.Fatalf("Test_ValidateProxyCertificate test case failed. Expected error %s but got %s", expectedResult, err.Error())
 	}
 }
