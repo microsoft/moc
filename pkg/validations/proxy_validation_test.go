9e236a970eb91dc8180bcd02ea630ce8cabf325c
Dhwani Shah <dhwanishah@microsoft.com>
2023-10-27 10:32:00 -0700

    removing commented code

--- pkg/validations/proxy_validation_test.go
+++ pkg/validations/proxy_validation_test.go
@@ -34,21 +34,6 @@ func Test_ValidateProxyURL(t *testing.T) {
 	if err.Error() != expectedResult {
 		t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s but got %s", expectedResult, err.Error())
 	}
-
-	// Commenting out below 2 unit tests as the sky proxy server is unreachable from ADO machines and wsl
-
-	// // Unreachable proxy server
-	// err = ValidateProxyURL("http://ubuntu:ubuntu@192.168.200.200:3128", "")
-	// expectedResult = "Get \"https://mcr.microsoft.com\": proxyconnect tcp: dial tcp 192.168.200.200:3128:"
-	// if !strings.Contains(err.Error(), expectedResult) {
-	// 	t.Fatalf("Test_ValidateProxyURL test case failed. Expected error %s", err.Error())
-	// }
-
-	// // Valid case
-	// err = ValidateProxyURL("http://akse2e:akse2e@skyproxy.ceccloud1.selfhost.corp.microsoft.com:3128", caCertString)
-	// if err != nil {
-	// 	t.Fatalf("Test_ValidateProxyURL test case failed. %s", err.Error())
-	// }
 }
 
 func Test_ValidateProxyCertificate(t *testing.T) {
