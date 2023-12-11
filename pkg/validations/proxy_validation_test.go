1f207bc34cc4e9ef17d9a96748ba2ffa383769ce
vlappenbusch <vlappenbusch@vivianlapp.redmond.corp.microsoft.com>
2023-12-11 11:16:01 -0800

    fix failing test

--- pkg/validations/proxy_validation_test.go
+++ pkg/validations/proxy_validation_test.go
@@ -38,7 +38,7 @@ func Test_TestProxyUrlConnection(t *testing.T) {
 	parsedUrl, _ := ValidateProxyURL("http://w3proxy.netscape.com:3128")
 	// Invalid hostname
 	err = TestProxyUrlConnection(parsedUrl, caCertString, "")
-	expectedResult := "Get \"https://mcr.microsoft.com\": proxyconnect tcp: dial tcp: lookup w3proxy.netscape.com: no such host: Invalid Input"
+	expectedResult := "Get \"https://mcr.microsoft.com\": proxyconnect tcp: dial tcp: lookup w3proxy.netscape.com on 172.20.48.1:53: no such host: Invalid Input"
 	if err.Error() != expectedResult {
 		t.Fatalf("Test_TestProxyUrlConnection test case failed. Expected error %s but got %s", expectedResult, err.Error())
 	}
