67f19d63e315428a561c7b2f78bdda130bf81764
damanMSFT <dtekchandani@microsoft.com>
2024-08-26 23:33:55 -0700

    Added a new Disk File Format

--- rpc/cloudagent/storage/moc_cloudagent_virtualharddisk.pb.go
+++ rpc/cloudagent/storage/moc_cloudagent_virtualharddisk.pb.go
@@ -328,7 +328,7 @@ func (m *VirtualHardDisk) GetDiskFileFormat() common.DiskFileFormat {
 	if m != nil {
 		return m.DiskFileFormat
 	}
-	return common.DiskFileFormat_DiskFileFormatVHDX
+	return common.DiskFileFormat_DiskFileFormatUNKNOWN
 }
 
 func (m *VirtualHardDisk) GetCloudInitDataSource() common.CloudInitDataSource {
