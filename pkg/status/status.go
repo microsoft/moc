aa9c5155170c5f6478c84ef15270535ac6910bc8
Niko Hobart <nikohobart@gmail.com>
2024-08-29 17:15:45 -0700

    revert prev change, unclear that that was RC

--- pkg/status/status.go
+++ pkg/status/status.go
@@ -102,13 +102,10 @@ func GetValidationStatus(s *common.Status) []*common.ValidationState {
 func GetStatuses(status *common.Status) map[string]*string {
 	statuses := map[string]*string{}
 
-	// Provision and Health State require custom parsing as they are enums.
-	// Otherwise enum 0 (UNKNOWN / NOT_KNOWN) will be an empty string.
 	pstate := parseProvisioning(status.GetProvisioningStatus())
 	statuses["ProvisionState"] = &pstate
 	hstate := parseHealth(status.GetHealth())
 	statuses["HealthState"] = &hstate
-
 	estate := status.GetLastError().String()
 	statuses["Error"] = &estate
 	version := status.GetVersion().Number
@@ -144,40 +141,4 @@ func GetFromStatuses(statuses map[string]*string) (status *common.Status) {
 	}
 
 	return
-}
-
-// HealthState requires custom parsing as it is a proto enum. Otherwise enum 0 (NOT_KNOWN) will be an empty string.
-func parseHealth(hstate *common.Health) string {
-	if hstate == nil {
-		return ""
-	}
-
-	prevHealth, ok := common.HealthState_name[int32(hstate.GetPreviousState())]
-	if !ok {
-		prevHealth = ""
-	}
-	currHealth, ok := common.HealthState_name[int32(hstate.GetCurrentState())]
-	if !ok {
-		currHealth = ""
-	}
-
-	return fmt.Sprintf("currentState:%s previousState:%s", currHealth, prevHealth)
-}
-
-// ProvisionState requires custom parsing as it is a proto enum. Otherwise enum 0 (UNKNOWN) will be an empty string.
-func parseProvisioning(pstate *common.ProvisionStatus) string {
-	if pstate == nil {
-		return ""
-	}
-
-	prevProv, ok := common.ProvisionState_name[int32(pstate.GetPreviousState())]
-	if !ok {
-		prevProv = ""
-	}
-	currProv, ok := common.ProvisionState_name[int32(pstate.GetCurrentState())]
-	if !ok {
-		currProv = ""
-	}
-
-	return fmt.Sprintf("currentState:%s previousState:%s", currProv, prevProv)
 }
