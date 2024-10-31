cc08c43d0cda581543762705b5771ed64676308d
Ruinan Liu <72321026+ruinan-liu@users.noreply.github.com>
2024-10-31 12:03:03 -0700

    [moc] Adding ChmodRecursivelyAdmin only function (#317)
    * Adding ChmodRecursiveAdmin function to set the path for admin access only
    
    * Adding command injection checks
    
    * Adding 2 more checks for command injection

--- pkg/fs/fs_darwin.go
+++ pkg/fs/fs_darwin.go
@@ -4,8 +4,26 @@ package fs
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func Chmod(path string, mode os.FileMode) error {
 	return os.Chmod(path, mode)
+}
+
+func ChmodRecursiveAdmin(path string) error {
+	err := filepath.Walk(path, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		if walkErr = Chmod(path, 0700); walkErr != nil {
+			return walkErr
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
