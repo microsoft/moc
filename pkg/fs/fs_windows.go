df26b76bba557606c3d8d1aeeae3213f05cdef3e
Ruinan Liu <ruinanliu@yahoo.com>
2024-10-31 11:12:25 -0700

    Adding ChmodRecursiveAdmin function to set the path for admin access only

--- pkg/fs/fs_windows.go
+++ pkg/fs/fs_windows.go
@@ -4,10 +4,28 @@ package fs
 
 import (
 	"os"
+	"os/exec"
 
 	"github.com/hectane/go-acl"
 )
 
 func Chmod(path string, mode os.FileMode) error {
 	return acl.Chmod(path, mode)
+}
+
+func ChmodRecursiveAdmin(path string) error {
+	// Step1: Remove inhereted permissions
+	cmd := exec.Command("icacls", path, "/inheritance:r")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	// Step2: Grant admin permission to the directory
+	cmd = exec.Command("icacls", path, "/grant", "BUILTIN\\Administrators:(OI)(CI)(F)")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	return nil
 }
