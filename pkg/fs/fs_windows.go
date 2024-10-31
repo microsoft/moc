9d16469da103b54136a53e7588e117e952638974
Ruinan Liu <72321026+ruinan-liu@users.noreply.github.com>
2024-10-31 12:03:03 -0700

    [moc] Adding ChmodRecursivelyAdmin only function (#317)
    * Adding ChmodRecursiveAdmin function to set the path for admin access only
    
    * Adding command injection checks
    
    * Adding 2 more checks for command injection

--- pkg/fs/fs_windows.go
+++ pkg/fs/fs_windows.go
@@ -4,10 +4,37 @@ package fs
 
 import (
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/hectane/go-acl"
+	"github.com/microsoft/moc/pkg/errors"
 )
 
 func Chmod(path string, mode os.FileMode) error {
 	return acl.Chmod(path, mode)
+}
+
+func ChmodRecursiveAdmin(path string) error {
+	// Step 0: check for command injections because we using exec command to run icacls
+	var err error
+	if strings.Contains(path, "&") || strings.Contains(path, "|") || strings.Contains(path, ";") || strings.Contains(path, "^") || strings.Contains(path, ">") {
+		err = errors.Wrapf(errors.InvalidInput, "Path [%s] contains invalid operators like '&', '|', ';', '^', '>'", path)
+		return err
+	}
+
+	// Step 1: Remove inhereted permissions
+	cmd := exec.Command("icacls", path, "/inheritance:r")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	// Step 2: Grant admin permission to the directory
+	cmd = exec.Command("icacls", path, "/grant", "BUILTIN\\Administrators:(OI)(CI)(F)")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	return nil
 }
