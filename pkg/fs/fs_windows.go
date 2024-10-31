e868698a4bbd1c20804092ad06a3fb61cf2ee67d
Ruinan Liu <ruinanliu@yahoo.com>
2024-10-31 11:30:59 -0700

    Adding command injection checks

--- pkg/fs/fs_windows.go
+++ pkg/fs/fs_windows.go
@@ -5,8 +5,10 @@ package fs
 import (
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/hectane/go-acl"
+	"github.com/microsoft/moc/pkg/errors"
 )
 
 func Chmod(path string, mode os.FileMode) error {
@@ -14,14 +16,21 @@ func Chmod(path string, mode os.FileMode) error {
 }
 
 func ChmodRecursiveAdmin(path string) error {
-	// Step1: Remove inhereted permissions
+	// Step 0: check for command injections because we using exec command to run icacls
+	var err error
+	if strings.Contains(path, "&") || strings.Contains(path, "|") || strings.Contains(path, ";") {
+		err = errors.Wrapf(errors.InvalidInput, "Path [%s] contains invalid operators like '&', '|', ';'", path)
+		return err
+	}
+
+	// Step 1: Remove inhereted permissions
 	cmd := exec.Command("icacls", path, "/inheritance:r")
-	_, err := cmd.CombinedOutput()
+	_, err = cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
 
-	// Step2: Grant admin permission to the directory
+	// Step 2: Grant admin permission to the directory
 	cmd = exec.Command("icacls", path, "/grant", "BUILTIN\\Administrators:(OI)(CI)(F)")
 	_, err = cmd.CombinedOutput()
 	if err != nil {
