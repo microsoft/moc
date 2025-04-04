efec9a6058137f0fc41a81471c02d7c48d30ecb9
Dhwani Shah <dhwanishah@microsoft.com>
2025-04-04 14:09:25 -0700

    adding default logger

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -4,6 +4,7 @@
 package logging
 
 import (
+	"fmt"
 	"github.com/go-logr/logr"
 )
 
@@ -15,6 +16,9 @@ type Logger interface {
 type LogrLogger struct {
 	logger logr.Logger
 }
+
+// DefaultLogger is a simple logger that writes to stdout
+type DefaultLogger struct{}
 
 // NewLogrLogger creates a new LogrLogger instance that wraps a logr.Logger
 func NewLogrLogger(logger logr.Logger) *LogrLogger {
@@ -31,4 +35,17 @@ func (l *LogrLogger) Info(msg string, keysAndValues ...interface{}) {
 // Error logs an error level message
 func (l *LogrLogger) Error(err error, msg string, keysAndValues ...interface{}) {
 	l.logger.Error(err, msg, keysAndValues...)
+}
+
+func (d *DefaultLogger) Info(msg string, keysAndValues ...interface{}) {
+	fmt.Printf("INFO: %s ", msg)
+	fmt.Println(keysAndValues...)
+}
+
+func (d *DefaultLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	fmt.Printf("ERROR: %s ", msg)
+	fmt.Println(keysAndValues...)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
 }
--- pkg/logging/logger_test.go
+++ pkg/logging/logger_test.go
@@ -1,7 +1,10 @@
 package logging
 
 import (
+	"bytes"
 	"errors"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 
 	"github.com/go-logr/logr/testr"
@@ -20,4 +23,45 @@ func TestLogrLogger_Error(t *testing.T) {
 
 	err := errors.New("test error")
 	logrLogger.Error(err, "test error message", "key3", true, "key4", 456.78)
+}
+
+func TestDefaultLogger_Info(t *testing.T) {
+	// Capture stdout
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	logger := &DefaultLogger{}
+	logger.Info("test info message", "key1", "value1", "key2", 123)
+
+	w.Close()
+	os.Stdout = oldStdout
+
+	var buf bytes.Buffer
+	buf.ReadFrom(r)
+	output := buf.String()
+
+	expectedOutput := "INFO: test info message key1 value1 key2 123\n"
+	assert.Equal(t, expectedOutput, output)
+}
+
+func TestDefaultLogger_Error(t *testing.T) {
+	// Capture stdout
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	logger := &DefaultLogger{}
+	err := errors.New("test error")
+	logger.Error(err, "test error message", "key3", true, "key4", 456.78)
+
+	w.Close()
+	os.Stdout = oldStdout
+
+	var buf bytes.Buffer
+	buf.ReadFrom(r)
+	output := buf.String()
+
+	expectedOutput := "ERROR: test error message key3 true key4 456.78\nError: test error\n"
+	assert.Equal(t, expectedOutput, output)
 }
